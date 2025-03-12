be6d01890a5f514414edff4d1c30ed411dd07860
sudeep prajapati <sudeeppprajapati@gmail.com>
2025-03-12 19:55:17 +0530

    Refactor API client and authentication logic for improved readability and error handling

--- models/User.ts
+++ models/User.ts
@@ -2,32 +2,26 @@ import mongoose, { Schema, model, models } from "mongoose";
 import bcrypt from "bcryptjs";
 
 export interface IUser {
-    email: string;
-    password: string;
-    _id?: mongoose.Types.ObjectId;
-    timestamp?: Date;
+  email: string;
+  password: string;
+  _id?: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
-const userSchema = new Schema<IUser>({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-    }
-}, { timestamps: true });
-
+const userSchema = new Schema<IUser>(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
-    if (this.isModified("password")) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 const User = models?.User || model<IUser>("User", userSchema);
